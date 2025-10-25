45ceeae6b0419ecb81b311deeba9e967c93ac25a
Typezera <willianbardela2131@hotmail.com>
2025-10-25 16:49:43 -0300

    feat: criação da rota getAllUsers() e implementação manual de retorno de DTO

--- src/users/dto/read_user_dto.ts
+++ src/users/dto/read_user_dto.ts
@@ -1,6 +1,14 @@
+import { Exclude, Expose } from 'class-transformer';
 export class ReadUserDto {
+  @Expose()
   id: string;
+  @Expose()
   name: string;
+  @Expose()
   email: string;
+  @Expose()
+  phone: string;
+  @Expose()
+  createdAt: Date;
   //jwt <-- future
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import {
   Controller,
-  //Get,
+  Get,
   Post,
   Body,
   //Param,
   UseInterceptors,
   ClassSerializerInterceptor,
+  NotFoundException,
   //HttpStatus,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
@@ -14,7 +15,7 @@ import { ReadUserDto } from './dto/read_user_dto';
 import { User } from './entities/user.entity';
 import { plainToInstance } from 'class-transformer';
 
-@UseInterceptors(ClassSerializerInterceptor)
+// @UseInterceptors(ClassSerializerInterceptor)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -24,6 +25,19 @@ export class UsersController {
     const createdUser: User = await this.usersService.create(createUserDto);
 
     return plainToInstance(ReadUserDto, createdUser, {
+      excludeExtraneousValues: true,
+    });
+  }
+
+  @Get()
+  async getAllUsers(): Promise<ReadUserDto[]> {
+    const allUsers: User[] = await this.usersService.findAll();
+
+    if (!allUsers || allUsers.length === 0) {
+      throw new NotFoundException(`Nenhum usuário cadastrado!.`);
+    }
+
+    return plainToInstance(ReadUserDto, allUsers, {
       excludeExtraneousValues: true,
     });
   }
